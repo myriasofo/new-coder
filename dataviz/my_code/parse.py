791d1f76229de2b7d661316383325eb3f38ad083
abe <myriasofo@gmail.com>
2017-11-28 17:01:40 -0500

    Refactor parsing

--- dataviz/my_code/parse.py
+++ dataviz/my_code/parse.py
@@ -1,24 +1,24 @@
 
-#import collections
 import csv
-#import matplotlib.pyplot as plt
-#import numpy as np
 
-MY_FILE = "data/sample_sfpd_incident_all.csv"
+CSV_FILE_PATH = "data/sample_sfpd_incident_all.csv"
 
-def parse(raw_file, delimiter):
-    """Parses a raw CSV file to a JSON-like object"""
-
-    parsed_data = []
-    with open(raw_file) as f:
-        csv_data = csv.reader(f, delimiter=delimiter)
-        column_names = next(csv_data)
-        for row in csv_data:
-            parsed_data.append(dict(zip(column_names, row)))
-    return parsed_data
+def parseCsvAsJsonList(file_path):
+    jsonList = []
+    with open(file_path) as f:
+        csv_file = csv.reader(f, delimiter=',')
+        column_names = next(csv_file)
+        for row in csv_file:
+            column_row_tuples = zip(column_names, row)
+            column_row_json = dict(column_row_tuples) 
+            jsonList.append(column_row_json)
+    return jsonList
 
 def main():
-    parsed = parse(MY_FILE, ',')
+    parsed = parseCsvAsJsonList(CSV_FILE_PATH)
+    print('data type of output:', type(parsed))
+    print("data type of output's elements:", type(parsed[0]))
+    print(parsed[0])
     print(len(parsed))
 
 if __name__ == '__main__':
