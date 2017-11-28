cfec46d67880b9152d96e7ccb80a7576113a2d44
abe <myriasofo@gmail.com>
2017-11-28 17:05:34 -0500

    Refactor naming to be more pythonic

--- dataviz/my_code/extract_csv_as_json_list.py
+++ dataviz/my_code/extract_csv_as_json_list.py
@@ -3,19 +3,19 @@ import csv
 
 CSV_FILE_PATH = "data/sample_sfpd_incident_all.csv"
 
-def parseCsvAsJsonList(file_path):
-    jsonList = []
+def extract_csv_file_as_json_list(file_path):
+    json_list = []
     with open(file_path) as f:
         csv_file = csv.reader(f, delimiter=',')
         column_names = next(csv_file)
         for row in csv_file:
             column_row_tuples = zip(column_names, row)
             column_row_json = dict(column_row_tuples) 
-            jsonList.append(column_row_json)
-    return jsonList
+            json_list.append(column_row_json)
+    return json_list
 
 def main():
-    parsed = parseCsvAsJsonList(CSV_FILE_PATH)
+    parsed = extract_csv_file_as_json_list(CSV_FILE_PATH)
     print('data type of output:', type(parsed))
     print("data type of output's elements:", type(parsed[0]))
     print(parsed[0])
